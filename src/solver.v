715a5eaf56ef14f99f268f173f17a5fe15d7cef3
Marcus Alagar <mvala079@gmail.com>
2026-05-10 03:12:58 -0700

    Neatened `solver.v`, added more density
    Added helper submodule for solver and `localparam` to
    prepare for possibly allow user input to specify
    ODE params.

--- src/solver.v
+++ src/solver.v
@@ -23,60 +23,69 @@ module solver(
   input wire step
 );
 
+  localparam a =  2'sd0;
+  localparam b = -2'sd1;
+  localparam c =  2'sd1;
+  localparam d =  2'sd0;
+
   reg signed [7:0] old_x;
   always @(posedge clk) begin
     if (rst) old_x <= 8'sd0;
-    else old_x <= x;
+    else old_x <= xin;
   end
 
-  wire changed_coords = old_x != x;
+  wire changed_coords = old_x != xin;
 
-  reg signed [7:0] start_x;
-  reg signed [7:0] start_y;
+  reg signed [7:0] start_x, start_y;
+  wire signed [7:0] next_start_x, next_start_y;
   always @(posedge clk) begin
     if (rst) begin
-      start_x <= 7'sd4;
-      start_y <= 7'sd0;
+      start_x <= 8'sd4;
+      start_y <= 8'sd0;
     end else if (step) begin
-      // VGA playground doesn't seem to support arithmetic right shift :(
-      // start_x <= start_x - (start_y >>> 2);
-      // start_y <= start_y + (start_x >>> 2);
-      start_x <= start_x
-        + 0 * {start_x[7] ? 2'b11 : 2'b00, start_x[7:2]} 
-        + (-1) * {start_y[7] ? 2'b11 : 2'b00, start_y[7:2]};
-      start_y <= start_y
-        + {start_x[7] ? 2'b11 : 2'b00, start_x[7:2]}
-        + 0 * {start_y[7] ? 2'b11 : 2'b00, start_y[7:2]};
+      start_x <= next_start_x;
+      start_y <= next_start_y;
     end else begin
       start_x <= start_x;
       start_y <= start_y;
     end
   end
 
-  reg signed [7:0] curr_x;
-  reg signed [7:0] curr_y;
+  solver_step start_stepper(
+    .xin(start_x),
+    .yin(start_y),
+    .xout(next_start_x),
+    .yout(next_start_y),
+    .a(a),
+    .b(b),
+    .c(c),
+    .d(d)
+  );
+
+  reg signed [7:0] curr_x, curr_y;
+  wire signed [7:0] next_curr_x, next_curr_y;
   always @(posedge clk) begin
-    if (~rst_n) begin
-      curr_x <= 7'sd4;
-      curr_y <= 7'sd0;
-    end else if (changed_coords) begin
-      curr_x <= start_x 
-        + 0 * {start_x[7] ? 2'b11 : 2'b00, start_x[7:2]} 
-        + (-1) * {start_y[7] ? 2'b11 : 2'b00, start_y[7:2]};
-      curr_y <= curr_y
-        + {start_x[7] ? 2'b11 : 2'b00, start_x[7:2]}
-        + 0 * {start_y[7] ? 2'b11 : 2'b00, start_y[7:2]};
+    if (rst) begin
+      curr_x <= 8'sd4;
+      curr_y <= 8'sd0;
     end else begin
-      curr_x <= curr_x
-        + 0 * {curr_x[7] ? 2'b11 : 2'b00, curr_x[7:2]} 
-        + (-1) * {curr_y[7] ? 2'b11 : 2'b00, curr_y[7:2]};
-      curr_y <= curr_y
-        + {curr_x[7] ? 2'b11 : 2'b00, curr_x[7:2]}
-        + 0 * {curr_y[7] ? 2'b11 : 2'b00, curr_y[7:2]};
+      curr_x <= next_curr_x;
+      curr_y <= next_curr_y;
     end
   end
 
-  wire match_start = (xin == start_x) & (yin == curr_y);
+  solver_step curr_stepper(
+    .xin(changed_coords ? start_x : curr_x),
+    .yin(changed_coords ? start_y : curr_y),
+    .xout(next_curr_x),
+    .yout(next_curr_y),
+    .a(a),
+    .b(b),
+    .c(c),
+    .d(d)
+  );
+
+  wire match_start = (xin == start_x) & (yin == start_y);
   wire match_curr = (xin == curr_x) & (yin == curr_y);
   wire seen_point = (~changed_coords & match_curr) | (changed_coords & match_start);
 
@@ -92,5 +101,31 @@ module solver(
   end
 
   assign is_point = seen_point | (saved_seen & ~changed_coords);
+
+endmodule
+
+
+// Simple Euler's method for solving ODEs,
+// with a pretty bad timestep of 0.25
+// and even worse quantization
+module solver_step(
+  input wire signed [7:0] xin,
+  input wire signed [7:0] yin,
+  output wire signed [7:0] xout,
+  output wire signed [7:0] yout,
+  input wire signed [1:0] a,
+  input wire signed [1:0] b,
+  input wire signed [1:0] c,
+  input wire signed [1:0] d
+);
+  // VGA playground doesn't seem to support arithmetic right shift :(
+  // start_x <= start_x - (start_y >>> 2);
+  // start_y <= start_y + (start_x >>> 2);
+  assign xout = xin 
+                  + a * $signed({xin[7] ? 2'b11 : 2'b00, xin[7:2]}) 
+                  + b * $signed({yin[7] ? 2'b11 : 2'b00, yin[7:2]});
+  assign yout = yin 
+                  + c * $signed({xin[7] ? 2'b11 : 2'b00, xin[7:2]})
+                  + d * $signed({yin[7] ? 2'b11 : 2'b00, yin[7:2]});
 
 endmodule
